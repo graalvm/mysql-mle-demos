14d332d0979907484b3b25e45b297746835c082d
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2018-04-20 10:42:44 -0700

    String tokenization demo.

---
+++ 3-string-tokenization/README.md
@@ -0,0 +1,26 @@
+## String Tokenization and Token Frequency in the Database
+
+In this demo we tokenize tweets that are stored in the database and check the word
+frequency.
+
+First install all the required packages:
+```
+npm install
+```
+
+Then to execute the tokenization and frequency on the client execute:
+```
+node token_frequency.js
+```
+
+To deploy the same function to the database:
+```
+dbjs deploy  -c "localhost/test?port=3306" --database=mysql -u root --browserify-ignore-missing --password= --browserify-ignore-missing --browserify-ignore ./node_modules/cls-bluebird/lib/index.js --browserify-ignore ./node_modules/iconv-lite/lib/index.js token_frequency.js
+```
+
+In the client simply invoke:
+```
+call TOKEN_FREQUENCY_TOKENFREQUENCY();
+```
+
+Observe the difference in the query execution time and the execution time in the client.
---
+++ 3-string-tokenization/token_frequency.js
@@ -0,0 +1,42 @@
+const mysql = require('mysql2');
+const tokenize = require('natural/lib/natural/tokenizers/regexp_tokenizer');
+const mle = require('mle');
+
+function wordCount(xs) {
+  return xs.reduce(function(rv, x) {
+    if (!(x in rv))
+      rv[x] = 1;
+    else
+      rv[x] += 1;
+    return rv;
+  }, {});
+};
+function tokenFrequency() {
+  const connection = mysql.createConnection({
+     host     : 'localhost',
+     user     : 'root',
+     password : '',
+     database : 'test',
+     multipleStatements: true
+  });
+  connection.connect();
+  var tokenizer = new tokenize.WordTokenizer();
+  var query = connection.query('SELECT email from emails;', function (error, results, fields) {
+    if (error) throw error;
+    var count = wordCount(results.map(r => r.email)
+      .reduce((acc, x) => acc.concat(tokenizer.tokenize(x)), []));
+
+    // count token frequency
+    for (var token in count) {
+      connection.query('INSERT into token_frequency SET ?', {"token": token, "frequency": groupped[token]}, function (error, results, fields) {
+        if (error) throw error;
+      });
+    }
+    connection.end();
+  });
+}
+
+if (!mle.enabled()) {
+  tokenFrequency();
+}
+module.exports.tokenFrequency = tokenFrequency;
