c1af3c3ff7678f193abf03e172b8c00de2d13c7d
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2018-10-18 23:31:18 +0200

    Simplify database deletion

--- run-all.sh
+++ run-all.sh
@@ -1,11 +1,13 @@
-#! /bin/sh -x
+#! /bin/sh -x -e
 if [ "$#" -ne 2 ]; then
     echo "Required two parameters: (1) the MySQL client command and (2) the dbjs command.";
     exit 1
 fi
 
 # Set all the executables.
+DATABASE=demo
 MYSQL=$1
+MYSQL_DEMO="$MYSQL --database $DATABASE"
 DBJS=$2
 
 # Check if running in the current folder.
@@ -16,29 +18,29 @@ fi
 ROOT_DIR=$PWD
 
 # Drop all
-$MYSQL < $ROOT_DIR/seed-data/drop.sql
+echo "drop database if exists $DATABASE;" | $MYSQL
+echo "create database $DATABASE;" | $MYSQL
 
 # Demo 0
-$MYSQL < $ROOT_DIR/0-demo-hello-js/hello-js.sql
+$MYSQL_DEMO < $ROOT_DIR/0-demo-hello-js/hello-js.sql
 
 # Demo 1
-$MYSQL < $ROOT_DIR/seed-data/emails.sql
-$MYSQL < $ROOT_DIR/1-levenshtein-distance/levenshtein.sql
-$MYSQL < $ROOT_DIR/1-levenshtein-distance/levenshtein_js.sql
+$MYSQL_DEMO < $ROOT_DIR/seed-data/emails.sql
+$MYSQL_DEMO < $ROOT_DIR/1-levenshtein-distance/levenshtein.sql
+$MYSQL_DEMO < $ROOT_DIR/1-levenshtein-distance/levenshtein_js.sql
 
 # Demo 2
 cd $ROOT_DIR/2-email-validation/
 npm install
-$DBJS deploy -c "localhost/demo?port=3306" --database=mysql -u root --password= validator.ts
-echo "select email from emails where validator_isemail(email) = 1;" | $MYSQL
-cd $ROOT_DIR
+$DBJS deploy -c "localhost/$DATABASE?port=3306" --database=mysql -u root --password= validator.ts
+echo "select email from emails where validator_isemail(email) = 1;" | $MYSQL_DEMO
 
 # Demo 3
-$MYSQL < $ROOT_DIR/seed-data/tweets.sql
-$MYSQL < $ROOT_DIR/seed-data/tokens.sql
+$MYSQL_DEMO < $ROOT_DIR/seed-data/tweets.sql
+$MYSQL_DEMO < $ROOT_DIR/seed-data/tokens.sql
 
 cd $ROOT_DIR/3-string-tokenization/
 npm install
-$DBJS deploy  -c "localhost/demo?port=3306" --database=mysql -u root --password=  --webpackConfig webpack.config.js tokens.js
-echo "call tokens_token_count(100);" | $MYSQL
-cd $ROOT_DIR
+$DBJS deploy -c "localhost/$DATABASE?port=3306" --database=mysql -u root --password=  --webpackConfig webpack.config.js tokens.js
+echo "call tokens_token_count(100);" | $MYSQL_DEMO
+
